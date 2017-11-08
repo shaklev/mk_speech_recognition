c8d25015dcb5c6be92b0609b453cd3bcae668ac3
jme1991 <jme1991@localhost.localdomain>
2017-11-08 19:45:23 +0100

    update blob file handle

--- mk_speech_recognition/views.py
+++ mk_speech_recognition/views.py
@@ -1,8 +1,16 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.views import View
 from django.shortcuts import render_to_response
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
 
+@method_decorator(csrf_exempt, name='dispatch')
 class MainView(View):
 
     def get(self, request):
         return render_to_response('main.html')
+    
+    def post(self, request):
+        print(request.FILES['audio'].read(1024))
+        data = {'status':200}
+        return JsonResponse(data)
