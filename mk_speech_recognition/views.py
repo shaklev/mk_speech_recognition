5164a4e9204f5e02cf3633d4f5594a5e7bb6fc50
jme1991 <jme1991@localhost.localdomain>
2017-11-08 22:15:51 +0100

    wiki api connect

--- mk_speech_recognition/views.py
+++ mk_speech_recognition/views.py
@@ -3,6 +3,7 @@ from django.views import View
 from django.shortcuts import render_to_response
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
+from wikiapi import WikiApi
 
 @method_decorator(csrf_exempt, name='dispatch')
 class MainView(View):
@@ -11,6 +12,8 @@ class MainView(View):
         return render_to_response('main.html')
     
     def post(self, request):
-        print(request.FILES['audio'].read(1024))
+        wiki = WikiApi({'locale':'mk'})
+        results = wiki.find('Гоце Делчев')
+        response = wiki.get_article(results[0]).summary
         data = {'status':200}
         return JsonResponse(data)
